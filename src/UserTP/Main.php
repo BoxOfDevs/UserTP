5abafbe910db64ca4d3639fa76273b508cf9d314
Ad5001 <ad5001yt@gmail.com>
2016-05-02 19:44:10 +0200

    Adding accept and decline

--- src/UserTP/Main.php
+++ src/UserTP/Main.php
@@ -34,7 +34,7 @@ use pocketmine\utils\Config;
 							if ($sender instanceof Player){
 								 if (count($args) < 2) {
 									 if (trim(strtolower($sender->getName())) === trim(strtolower($args[1]))) {
-										 $sender->sendMessage(TextFormat::RED . " Why are you trying to teleport to yourself?");
+										 $sender->sendMessage(TextFormat::RED . "Why are you trying to teleport to yourself?");
 										 return true;
 									 }
 									 if ($this->getServer()->getPlayer($args[1]) instanceof Player) {
@@ -43,16 +43,16 @@ use pocketmine\utils\Config;
 										 $this->tp_reciver[$sender->getName()] = $reciever->getName();
 										 $this->getServer()->getPlayer($this->tp_reciver[$sender->getName()])->sendMessage(TextFormat::GOLD . $sender->getName() . TextFormat::WHITE . " request to teleport to you !\n Type /tpa accept $sender->getName() to teleport him to you or /tpa decline to decline the teleportation");
 								    } else {
-										$sender->sendMessage(TextFormat::RED . $this->config->get("Player $args[1] does NOT exist ! Don't try to teleport to a ghost !"));
+										$sender->sendMessage(TextFormat::RED . "Player $args[1] does NOT exist ! Don't try to teleport to a ghost !");
 										return true;
 									}
 									} else {
-										$sender->sendMessage(TextFormat::RED . $this->config->get("WHo are you trying to teleport to?"));
+										$sender->sendMessage(TextFormat::RED ."Who are you trying to teleport to?");
 										return false;
 									}
 
 							 } else {
-								 $sender->sendMessage(TextFormat::RED.$this->config->get("Did you think that you can tp a RCON?"));
+								 $sender->sendMessage(TextFormat::RED."Did you think that you can tp a RCON?");
 								 return true;
 							 }
 							return true;
@@ -62,7 +62,7 @@ use pocketmine\utils\Config;
 							if ($sender instanceof Player){
 								 if (count($args) < 2) {
 									 if (trim(strtolower($sender->getName())) === trim(strtolower($this->getServer()->getPlayer($args[1])->getName()))) {
-										 $sender->sendMessage(TextFormat::RED . " Why are you trying to teleport to yourself?");
+										 $sender->sendMessage(TextFormat::RED . "Why are you trying to teleport to yourself?");
 										 return true;
 									 }
 									 if ($this->getServer()->getPlayer($args[1]) instanceof Player) {
@@ -71,20 +71,22 @@ use pocketmine\utils\Config;
 										 $this->tp_invite_reciver[$sender->getName()] = $reciever->getName();
 										 $this->getServer()->getPlayer($this->tp_invite_reciver[$sender->getName()])->sendMessage(TextFormat::GOLD . $sender->getName() . TextFormat::WHITE . " request you to teleport to him !\n Type /tpa accept $sender->getName() to teleport to him or /tpa decline to decline the teleportation");
 								    } else {
-										$sender->sendMessage(TextFormat::RED . $this->config->get("Player $args[1] does NOT exist ! Don't try to teleport to a ghost !"));
+										$sender->sendMessage(TextFormat::RED . "Player $args[1] does NOT exist ! Don't try to teleport to a ghost !");
 										return true;
 									}
 									} else {
-										$sender->sendMessage(TextFormat::RED . $this->config->get("Who are you trying to teleport to?"));
+										$sender->sendMessage(TextFormat::RED . "Who are you trying to teleport to?");
 										return false;
 									}
 
 							 } else {
-								 $sender->sendMessage(TextFormat::RED.$this->config->get("Did you think that you can tp to  a RCON?"));
+								 $sender->sendMessage(TextFormat::RED."Did you think that you can tp to  a RCON?");
 								 return true;
 							 }
 							return true;
 							break;
+							
+							
 							case "accept":
 							if ($sender instanceof Player){
 								 if (count($args) < 2) {
@@ -92,24 +94,76 @@ use pocketmine\utils\Config;
 										 $sender->sendMessage("No one are trying to teleport to you / to him.");
 										 return true;
 									 }
-									 if(isset($this->tp_sender[$sender->getName()])) {
+									 
+									 
+									 if(isset($this->tp_sender[$sender->getName()])) { // if it's an ask
 									 if ($this->getServer()->getPlayer($this->tp_sender[$sender->getName()]) instanceof Player) {
-										 $reciever = $this->getServer()->getPlayer($args[1]);
-										 $this->tp_invite_sender[$reciever->getName()]  = $sender->getName();
-										 $this->tp_invite_reciver[$sender->getName()] = $reciever->getName();
-										 $this->getServer()->getPlayer($this->tp_invite_reciver[$sender->getName()])->sendMessage(TextFormat::GOLD . $sender->getName() . TextFormat::WHITE . " request you to teleport to him !\n Type /tpa accept $sender->getName() to teleport to him or /tpa decline to decline the teleportation");
-								    } else {
-										$sender->sendMessage(TextFormat::RED . $this->config->get("Player $args[1] does NOT exist ! Don't try to teleport to a ghost !"));
-										return true;
-									}
+										 $tpsender = $this->getServer()->getPlayer($this->tp_sender[$sender->getName()]);
+										 $tpsender->teleport(new Vector3($sender->x, $sender->y, $sender->z));
+										 unset($this->tp_sender[$sender->getName]);
+										 unset($this->tp_reciver[$tpsender->getName]);
+										 $sender->sendMessage(TextFormat::GOLD . $tpsender->getName() . TextFormat::GREEN . " succefully teleported to you");
+										 $tpsender->sendMessage(TextFormat::GREEN."You have been teleported to ".TextFormat::GOLD . $sender->getName());
+									 }
+									 
+									 
+									 } elseif(isset($this->tp_invite_sender[$sender->getName()])) { // if it's an invite
+									 if ($this->getServer()->getPlayer($this->tp_invite_sender[$sender->getName()]) instanceof Player) {
+										 $tpsender = $this->getServer()->getPlayer($this->tp_invite_sender[$sender->getName()]);
+										 $sender->teleport(new Vector3($tpsender->x, $tpsender->y, $tpsender->z));
+										 unset($this->tp_invite_sender[$sender->getName]);
+										 unset($this->tp_invite_reciver[$tpsender->getName]);
+										 $tpsender->sendMessage(TextFormat::GOLD . $sender->getName() . TextFormat::GREEN . " teleported to you");
+										 $sender->sendMessage(TextFormat::GREEN."You succefully teleported to ".TextFormat::GOLD . $tpsender->getName());
+									 }
 									 }
 									} else {
-										$sender->sendMessage(TextFormat::RED . $this->config->get("Who are you trying to teleport to?"));
+										$sender->sendMessage(TextFormat::RED . "Who are you trying to teleport to?");
 										return false;
 									}
 
 							 } else {
-								 $sender->sendMessage(TextFormat::RED.$this->config->get("Did you think that you can tp to  a RCON?"));
+								 $sender->sendMessage(TextFormat::RED."Did you think that you can tp to  a RCON?");
+								 return true;
+							 }
+							return true;
+							break;
+							
+							case "decline":
+							if ($sender instanceof Player){
+								 if (count($args) < 2) {
+									 if (!isset($this->tp_sender[$sender->getName()]) or !isset($this->tp_invite_sender[$sender->getName()])) {
+										 $sender->sendMessage("No one are trying to teleport to you / to him.");
+										 return true;
+									 }
+									 
+									 
+									 if(isset($this->tp_sender[$sender->getName()])) { // if it's an ask
+									 if ($this->getServer()->getPlayer($this->tp_sender[$sender->getName()]) instanceof Player) {
+										 $tpsender = $this->getServer()->getPlayer($this->tp_sender[$sender->getName()]);
+										 unset($this->tp_sender[$sender->getName]);
+										 unset($this->tp_reciver[$tpsender->getName]);
+										 $sender->sendMessage(TextFormat::GREEN."You have declined teleport request of ".TextFormat::GOLD . $sender->getName());
+										 $tpsender->sendMessage(TextFormat::GOLD . $sender->getName() . TextFormat::GREEN . " declined your teleport request");
+									 }
+									 
+									 
+									 } elseif(isset($this->tp_invite_sender[$sender->getName()])) { // if it's an invite
+									 if ($this->getServer()->getPlayer($this->tp_invite_sender[$sender->getName()]) instanceof Player) {
+										 $tpsender = $this->getServer()->getPlayer($this->tp_invite_sender[$sender->getName()]);
+										 unset($this->tp_invite_sender[$sender->getName]);
+										 unset($this->tp_invite_reciver[$tpsender->getName]);
+										 $sender->sendMessage(TextFormat::GREEN."You have declined teleport request of ".TextFormat::GOLD . $sender->getName());
+										 $tpsender->sendMessage(TextFormat::GOLD . $sender->getName() . TextFormat::GREEN . " declined your teleport request");
+									 }
+									 }
+									} else {
+										$sender->sendMessage(TextFormat::RED . "Who are you trying to teleport to?");
+										return false;
+									}
+
+							 } else {
+								 $sender->sendMessage(TextFormat::RED."Did you think that you can tp to  a RCON?");
 								 return true;
 							 }
 							return true;
